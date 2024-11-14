1e63df9de54377abbd631965d0f5b48bce3499fb
anish.babu <anish.babu@caci.com>
2024-11-14 12:21:42 -0500

    Filter logic added

--- src/main/java/com/wingmanapp/wingmanapp_backend/controller/GoogleMapsProxyController.java
+++ src/main/java/com/wingmanapp/wingmanapp_backend/controller/GoogleMapsProxyController.java
@@ -6,30 +6,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class GoogleMapsProxyController {
 
     private final String BASE_URL_NEARBY = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
     private final String BASE_URL_PHOTO = "https://maps.googleapis.com/maps/api/place/photo";
-    private final String BASE_URL_DETAIL = "https://maps.googleapis.com/maps/api/place/details";
+    private final String BASE_URL_DETAIL = "https://maps.googleapis.com/maps/api/place/details/json";
+
+    private final String[] categoryGroups = {
+            "restaurant|cafe|bar|bakery|meal_delivery|meal_takeaway",
+            "museum|art_gallery|movie_theater|bowling_alley|zoo|amusement_park|aquarium|night_club|casino|spa",
+            "park|shopping_mall|book_store|clothing_store|shoe_store|jewelry_store|store|florist|beauty_salon|library",
+            "campground|city_hall|embassy|stadium|tourist_attraction|university"
+    };
 
     @GetMapping("/api/proxy/nearbysearch")
-    public ResponseEntity<String> proxyNearbySearch(
+    public ResponseEntity<Map<String, Object>> proxyNearbySearch(
             @RequestParam double latitude,
             @RequestParam double longitude,
             @RequestParam int radius,
             @RequestParam String apiKey,
-            @RequestParam(required = false) String pageToken
+            @RequestParam(required = false) String nextPageToken,
+            @RequestParam(required = false, defaultValue = "0") int groupIndex
     ) {
-        String url = String.format("%s?location=%f,%f&radius=%d&key=%s", BASE_URL_NEARBY, latitude, longitude, radius, apiKey);
-        if (pageToken != null && !pageToken.isEmpty()) {
-            url += "&pagetoken=" + pageToken;
+        String keywords = categoryGroups[groupIndex];
+        String url = String.format("%s?location=%f,%f&radius=%d&keyword=%s&key=%s",
+                BASE_URL_NEARBY, latitude, longitude, radius, keywords, apiKey);
+
+        if (nextPageToken != null && !nextPageToken.isEmpty()) {
+            url += "&pagetoken=" + nextPageToken;
         }
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+        Map<String, Object> responseBody = handleApiResponse(response, groupIndex);
 
-        return ResponseEntity.ok().body(response.getBody());
+        return ResponseEntity.ok(responseBody);
     }
 
     @GetMapping("/api/proxy/detail")
@@ -37,7 +52,7 @@ public class GoogleMapsProxyController {
             @RequestParam String placeId,
             @RequestParam String apiKey
     ) {
-        String url = String.format("%s/json?place_id=%s&key=%s", BASE_URL_DETAIL, placeId, apiKey);
+        String url = String.format("%s?place_id=%s&key=%s", BASE_URL_DETAIL, placeId, apiKey);
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
@@ -59,5 +74,17 @@ public class GoogleMapsProxyController {
         return ResponseEntity
                 .status(response.getStatusCode())
                 .body(response.getBody());
+    }
+
+    private Map<String, Object> handleApiResponse(ResponseEntity<Map> response, int groupIndex) {
+        Map<String, Object> responseBody = new HashMap<>(response.getBody());
+        if (responseBody.containsKey("next_page_token")) {
+            responseBody.put("groupIndex", groupIndex);
+        } else if (groupIndex < categoryGroups.length - 1) {
+            responseBody.put("groupIndex", groupIndex + 1);
+        } else {
+            responseBody.put("groupIndex", null);
+        }
+        return responseBody;
     }
 }
